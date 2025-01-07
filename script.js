2b4e9d5ce41e9d8522acf301b80201428f4f2281
Anko0004 <Anko0004@stud.kea.dk>
2025-01-07 11:20:41 +0100

    Java

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const menuLinks = document.querySelectorAll(".menu a");
+  //Koden udføres når HTML er indlæst
+  const menuLinks = document.querySelectorAll(".menu a"); // Vælger alle <a>-elementer i .menu og gemmer dem som en NodeList i variablen menuLinks
 
-  // Hent stien fra den nuværende URL
+  // Hent pathname "stien" fra den nuværende URL (fx "Om mig")
   const currentPath = window.location.pathname;
 
+  // Gennemgår hvert link i menuLinks (NodeList) ved hjælp af en forEach-loop
   menuLinks.forEach((link) => {
-    // Sammenlign kun stien uden protokollen og domænet
+    //Inde i forEach sammenlignes stien (pathname) for hvert link med den aktuelle sti (currentPath).
     if (link.pathname === currentPath) {
       link.classList.add("active"); // Tilføj 'active' klasse til det link, der svarer til den nuværende side
     }
